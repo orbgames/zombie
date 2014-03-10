02feef48d78d60af0a30b0446d68796d64f3cffc
Dmitry Vanidovsky <vanidovsky@gmail.com>
2014-03-10 02:37:12 +0200

    Fixing for puma socket

--- config/deploy/puma/production.rb
+++ config/deploy/puma/production.rb
@@ -2,6 +2,6 @@ environment 'production'
 
 threads 4, 16
 
-bind "unix:///var/www/zom/production/shared/puma.sock"
-state_path  "/var/www/zom/production/shared/puma.state"
-pidfile     "/var/www/zom/production/shared/puma.pid"
+bind "unix:///var/www/zom/production/shared/tmp/sockets/puma.sock"
+state_path  "/var/www/zom/production/shared/tmp/pids/puma.state"
+pidfile     "/var/www/zom/production/shared/tmp/pids/puma.pid"
